895512a3c44e9e25508aad8439dc03c23bd27fdb
Gianni <Gianni@DESKTOP-U8MLKLB>
2018-04-19 19:57:01 +0200

    hm_19042018

---
+++ GestioneFatture/src/it/exp75/gestionefatture/test/EmailValidator.java
@@ -0,0 +1,16 @@
+package it.exp75.gestionefatture.test;
+
+import java.util.regex.Pattern;
+
+public class EmailValidator {
+	
+	
+	public static boolean validate(final String emailAddress) {
+	    if ( emailAddress == null ) return false;
+	    String patternStr = "^[-!#$%&'*+/0-9=?A-Z^_a-z{|}~](\\.?[-!#$%&'*+/0-9=?A-Z^_a-z{|}~])*@[a-zA-Z](-?[a-zA-Z0-9])*(\\.[a-zA-Z](-?[a-zA-Z0-9])*)+$";
+	    Pattern emailPattern = Pattern.compile(patternStr);
+	    return emailPattern.matcher(emailAddress).matches();
+	  }
+	  
+	  
+}
--- GestioneFatture/src/it/exp75/gestionefatture/test/Test.java
+++ GestioneFatture/src/it/exp75/gestionefatture/test/Test.java
@@ -9,37 +9,50 @@ import java.util.Locale.Category;
 public class Test {
 
 	public static void main(String[] args) {
-		Locale italian = new Locale("it", "IT", "EURO");
-		Locale.setDefault(italian);
-		NumberFormat nf = NumberFormat.getCurrencyInstance();
-		System.out.println(nf.format(123));
+//		Locale italian = new Locale("it", "IT", "EURO");
+//		Locale.setDefault(italian);
+//		NumberFormat nf = NumberFormat.getCurrencyInstance();
+//		System.out.println(nf.format(123));
+//		
+//		DecimalFormat nf2 = (DecimalFormat)DecimalFormat.getNumberInstance(Locale.ITALIAN);
+//		System.out.println(nf2.format(123));
+///*
+// * 
+// */
+//		
+//		DecimalFormatSymbols formatSymbols = new DecimalFormatSymbols(Locale.getDefault());
+//		formatSymbols.setDecimalSeparator(',');
+//		formatSymbols.setGroupingSeparator('.');
+//
+//		DecimalFormat df = new DecimalFormat("####,####.##", formatSymbols);
+//
+//		System.out.println("Formatted decimal: " + df.format(4567));
+///*
+// * 		
+// */
+//		double d1 = 123456.78;
+//		double d2 = 567890;
+//		// self commenting issue, the code is easier to understand
+//		Locale fmtLocale = Locale.getDefault(Category.FORMAT);
+//		NumberFormat formatter = NumberFormat.getInstance(fmtLocale);
+//		formatter.setMaximumFractionDigits(2);
+//		formatter.setMinimumFractionDigits(2);
+//		System.out.println(formatter.format(d1));
+//		System.out.println(formatter.format(d2));
+//		System.out.println(fmtLocale.toLanguageTag());
+//		
+		String[] emailAddress = {"MM&MMM@HHHHH.COM","MM&d#ddd@HH23HH.COM","gluigi755@psdfsadf.adad.it","MM&safdsadgfdsagg@HHHHH.COM"};
 		
-		DecimalFormat nf2 = (DecimalFormat)DecimalFormat.getNumberInstance(Locale.ITALIAN);
-		System.out.println(nf2.format(123));
-/*
- * 
- */
+		for(String email : emailAddress) {
+
+			if(EmailValidator.validate(email)) {
+				System.out.println(email + ": Valida");
+			} else {
+				System.out.println(email + ": NON Valida");
+			}
+		}
 		
-		DecimalFormatSymbols formatSymbols = new DecimalFormatSymbols(Locale.getDefault());
-		formatSymbols.setDecimalSeparator(',');
-		formatSymbols.setGroupingSeparator('.');
-
-		DecimalFormat df = new DecimalFormat("####,####.##", formatSymbols);
-
-		System.out.println("Formatted decimal: " + df.format(4567));
-/*
- * 		
- */
-		double d1 = 123456.78;
-		double d2 = 567890;
-		// self commenting issue, the code is easier to understand
-		Locale fmtLocale = Locale.getDefault(Category.FORMAT);
-		NumberFormat formatter = NumberFormat.getInstance(fmtLocale);
-		formatter.setMaximumFractionDigits(2);
-		formatter.setMinimumFractionDigits(2);
-		System.out.println(formatter.format(d1));
-		System.out.println(formatter.format(d2));
-		System.out.println(fmtLocale.toLanguageTag());
 	}
+	
 
 }
