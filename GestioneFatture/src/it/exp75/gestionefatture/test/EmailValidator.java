98d2d5d04de56cb0b94d6638ebefff862554abab
Gianni <Gianni@DESKTOP-U8MLKLB>
2018-04-19 20:05:44 +0200

    hm_19042018_1

--- GestioneFatture/src/it/exp75/gestionefatture/test/EmailValidator.java
+++ GestioneFatture/src/it/exp75/gestionefatture/test/EmailValidator.java
@@ -4,13 +4,10 @@ import java.util.regex.Pattern;
 
 public class EmailValidator {
 	
-	
 	public static boolean validate(final String emailAddress) {
 	    if ( emailAddress == null ) return false;
 	    String patternStr = "^[-!#$%&'*+/0-9=?A-Z^_a-z{|}~](\\.?[-!#$%&'*+/0-9=?A-Z^_a-z{|}~])*@[a-zA-Z](-?[a-zA-Z0-9])*(\\.[a-zA-Z](-?[a-zA-Z0-9])*)+$";
 	    Pattern emailPattern = Pattern.compile(patternStr);
 	    return emailPattern.matcher(emailAddress).matches();
 	  }
-	  
-	  
 }
